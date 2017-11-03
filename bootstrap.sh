017af143f594ca9a8f7f98f8a6aa21cbdf2a168c
MD. Mahmud Ur Rahman <mahmudkuet11@gmail.com>
2017-11-03 18:01:16 +0600

    inital commit

---
+++ bootstrap.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+sudo apt-get update -y
+
+
+# Nginx
+
+sudo apt-get install nginx -y
+
+
+# PHP
+
+sudo add-apt-repository ppa:ondrej/php -y
+
+sudo apt-get update -y
+
+sudo apt-get install php7.1 php-fpm php-mysql php-xml -y
+
+sudo sed -ie "s/^;cgi.fix_pathinfo=1/cgi.fix_pathinfo = 0/g" /etc/php/7.1/fpm/php.ini
+
+sudo service php7.1-fpm restart
+
+
+#Debconf
+
+sudo apt-get install debconf-utils -y
+
+
+# MySql
+
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+
+sudo apt-get install mysql-server -y
+
+mysql -uroot -proot -e "create database maxxecom;"
+
+sudo sed -ie "s/.*bind-address.*/# bind-address = 127.0.0.1/g" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'maxxecom'@'%' IDENTIFIED BY '1234' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+sudo service mysql restart
+
+# Vhost
+
+sudo cp /var/www/config/nginx_vhost /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
+sudo rm -rf /etc/nginx/sites-available/default
+sudo service nginx restart
+
+
+# Node
+
+sudo apt-get install nodejs -y
+
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+sudo apt-get install npm -y
+
+
+# Redis
+
+sudo apt-get install redis-server -y
+
+
+# Permissions
+
+sudo chmod -R 777 /var/www/src/storage/
+sudo chmod -R 777 /var/www/src/bootstrap/cache/
+
+
+# Supervisor
+
+sudo apt-get install supervisor -y
+sudo mkdir /var/www/log
+sudo cp /var/www/config/maxxecom_worker.conf /etc/supervisor/conf.d/
+sudo cp /var/www/config/maxxecom_node_worker.conf /etc/supervisor/conf.d/
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start maxxecom-worker:*
+sudo supervisorctl start maxxecom-node-worker:*
